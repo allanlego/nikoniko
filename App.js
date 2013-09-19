9934f78abddd05e3f5da2b98a2065852a3fe7394
Allan Lykke Christensen <allan.lykke.christensen@lego.com>
2013-09-19 16:29:15 -0400

    Using appId instead of workspace for storing preferences. Using rally1 instead of sandbox as the sandbox was getting problems (javascript errors)

--- App.js
+++ App.js
@@ -1,128 +1,129 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
- 
-    // Remove this override in production (in-rally use)
-    getAppId : function() { return "14154311144"; },
-    getCurrentUser : function() {
+    items: [{
+        xtype: "container",
+        itemId: "outercontainer",
+        layout: {
+            type: "vbox"
+        },
+        items: [{
+            xtype: "container",
+            itemId: "labelcontainer",
+            layout: {
+                type: "hbox",
+                align: "stretch"
+            }
+        }, {
+            xtype: "container",
+            itemId: "buttoncontainer",
+            layout: {
+                type: "hbox",
+                align: "stretch"
+            }
+        }]
+    }],
+
+    launch: function () {
+        this._doButtonLayout();
+    },
+    
+    getCurrentUser: function () {
         return this.getContext().getUser().ObjectID;
     },
 
-    _encodeKey : function(date, user) {
-        return date + "/" + user; 
+    _encodeKey: function (date, user) {
+        return date + "/" + user;
     },
-    
-    items: [
-        {
-            xtype: "container",
-            itemId: "outercontainer",
-            layout: {
-                type: "vbox"
-            },
-            items: [
-                {
-                    xtype: "container",
-                    itemId: "labelcontainer",
-                    layout: {
-                        type: "hbox",
-                        align: "stretch"
-                    }
-                },
-                {
-                    xtype: "container",
-                    itemId: "buttoncontainer",
-                    layout: {
-                        type: "hbox",
-                        align: "stretch"
-                    }
-                }
-            ]
-        },
-    ],
 
-    launch: function() {
-        //Write app code here
-    //    this.getPreferencesByDateAndUser(today, user);
-        this._doButtonLayout();
-    
-    },
-    
-    _doButtonLayout : function() {
+    _doButtonLayout: function () {
         var today = Ext.Date.format(new Date(), "Y-m-d");
 
-        var button1 = Ext.create('Rally.ui.Button', {
-           text: "Good",
-           listeners: {
-               click: function(button, event) { 
-                   this.saveMood(today, this.getCurrentUser(), 3);
-               },
-               scope: this
-           },
-           
-        });
-        var button2 = Ext.create('Rally.ui.Button', {
-           text: "Neutral",
-           listeners: {
-               click: function(button, event) { 
-                   this.saveMood(today, this.getCurrentUser(), 2);
-               },
-               scope: this
-           },
-        });
-        var button3 = Ext.create('Rally.ui.Button', {
-           text: "Bad",
-           listeners: {
-               click: function(button, event) { 
-                   this.saveMood(today, this.getCurrentUser(), 1);
-               },
-               scope: this
-           },
-           
-        });
-    
-        
         var labelToday = Ext.create('Ext.Component', {
             html: today
         });
-        
-        
+
+        var button1 = Ext.create('Rally.ui.Button', {
+            text: "Good",
+            listeners: {
+                click: function (button, event) {
+                    this.saveMood(today, this.getCurrentUser(), 3);
+                },
+                scope: this
+            }
+        });
+        var button2 = Ext.create('Rally.ui.Button', {
+            text: "Neutral",
+            listeners: {
+                click: function (button, event) {
+                    this.saveMood(today, this.getCurrentUser(), 2);
+                },
+                scope: this
+            }
+        });
+        var button3 = Ext.create('Rally.ui.Button', {
+            text: "Bad",
+            listeners: {
+                click: function (button, event) {
+                    this.saveMood(today, this.getCurrentUser(), 1);
+                },
+                scope: this
+            }
+        });
+
+        var button4 = Ext.create('Rally.ui.Button', {
+            text: "Check",
+            listeners: {
+                click: function (button, event) {
+                    Ext.Msg.alert('Status', 'Check the console');
+
+                    this.getPreferencesByDateAndUser(today, this.getCurrentUser());
+                },
+                scope: this
+            }
+        });
+
         this.down('#labelcontainer').add(labelToday);
         this.down('#buttoncontainer').add(button1);
         this.down('#buttoncontainer').add(button2);
         this.down('#buttoncontainer').add(button3);
+        this.down('#buttoncontainer').add(button4);
     },
-    
-    getPreferencesByDateAndUser:function(date, user){
+
+    getPreferencesByDateAndUser: function (date, user) {
         console.log("Getting mood.....");
         Rally.data.PreferenceManager.load({
-            additionalFilters :[
-                {
-                    property: 'Name',
-                    operator: 'contains',
-                    value: this._encodeKey(date, user)
-                }
-            ],
-            workspace: this.getContext().getWorkspaceRef(),
-            scope:this,
-            success:function(preferences) { console.log("Got: ", preferences)} 
+            appID: this.getAppId(),
+            additionalFilters: [{
+                property: 'Name',
+                operator: 'contains',
+                value: this._encodeKey(date, user)
+            }],
+            scope: this,
+            success: function (preferences) {
+                console.log("Got: ", preferences);
+            }
         });
     },
 
     //adds one saves the preferences
-    saveMood:function(date, user, mood){
+    saveMood: function (date, user, mood) {
         console.log("Saving mood.....");
         var key = this._encodeKey(date, user);
         var value = mood;
-        
+
         var mysettings = {};
         mysettings[key] = value;
         Rally.data.PreferenceManager.update({
-            workspace: this.getContext().getWorkspaceRef(),
+            appID: this.getAppId(),
+            //workspace: this.getContext().getWorkspaceRef(),
             scope: this,
-            settings:mysettings,
-            success: function(updatedRecords, noUpdatedRecords) { console.log("Mood stored for " + user + " on " + date + ": " + mood); console.log(updatedRecords); console.log(noUpdatedRecords); }
+            settings: mysettings,
+            success: function (updatedRecords, noUpdatedRecords) {
+                console.log("Mood stored for " + user + " on " + date + ": " + mood);
+                console.log(updatedRecords);
+                console.log(noUpdatedRecords);
+            }
         });
-    },
-
-    
+    }
 });
