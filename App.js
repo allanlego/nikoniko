0e3dfa0caa3201da8c7b11a9d7767123b564d4db
Allan Lykke Christensen <allan.lykke.christensen@lego.com>
2013-09-19 10:15:43 -0400

    UI

--- App.js
+++ App.js
@@ -6,18 +6,81 @@ Ext.define('CustomApp', {
     _encodeKey : function(date, user) {
         return date + "/" + user; 
     },
+    
+    items: [
+        {
+            xtype: "container",
+            itemId: "outercontainer",
+            layout: {
+                type: "vbox"
+            },
+            items: [
+                {
+                    xtype: "container",
+                    itemId: "labelcontainer",
+                    layout: {
+                        type: "hbox",
+                        align: "stretch"
+                    }
+                },
+                {
+                    xtype: "container",
+                    itemId: "buttoncontainer",
+                    layout: {
+                        type: "hbox",
+                        align: "stretch"
+                    }
+                }
+            ]
+        },
+    ],
 
     launch: function() {
         //Write app code here
-        var today = "2013-09-19";
-        var user = "t424est2";
-        var mood = 2;
+    //    var today = "2013-09-19";
+    //    var user = "t424est2";
+    //    var mood = 2;
         
-        this.saveMood(today, user, mood);
-        this.getPreferencesByUserAndDate(today, user);
+    //    this.saveMood(today, user, mood);
+    //    this.getPreferencesByDateAndUser(today, user);
+        this._doButtonLayout();
+    
     },
     
-    getPreferencesByUserAndDate:function(date, user){
+    _doButtonLayout : function() {
+        var button1 = Ext.create('Rally.ui.Button', {
+           text: "Good",
+           listeners: {
+               click: function(button, event) { console.log(':-)');}
+           },
+        });
+        var button2 = Ext.create('Rally.ui.Button', {
+           text: "Neutral",
+           listeners: {
+               click: function(button, event) { console.log(':-|');}
+           },
+        });
+        var button3 = Ext.create('Rally.ui.Button', {
+           text: "Bad",
+           listeners: {
+               click: function(button, event) { console.log(':-(');}
+           },
+        });
+        
+        var today = Ext.Date.format(Ext.Date.now(), "Y-m-d");
+        
+        var labelToday = Ext.create('Ext.Component', {
+            html: today
+        });
+        
+        
+        this.down('#labelcontainer').add(labelToday);
+        this.down('#buttoncontainer').add(button1);
+        this.down('#buttoncontainer').add(button2);
+        this.down('#buttoncontainer').add(button3);
+    },
+    
+    getPreferencesByDateAndUser:function(date, user){
         console.log("Getting mood.....");
         Rally.data.PreferenceManager.load({
             additionalFilters :[
